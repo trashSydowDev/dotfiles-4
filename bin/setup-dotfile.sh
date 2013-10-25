b1a3913396e7b8796916491c73ff4fb976d9b663
yamadapc <tacla.yamada@gmail.com>
2013-10-25 05:50:26 -0200

    Add simple script for setting up my dotfiles - to prevent the dread of symlinking and etc.

---
+++ bin/setup-dotfile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# setup-dotfiles
+#
+# Author: yamadapc <github.com/yamadapc> 2013
+# Simple shell script to create the necessary symlinks to my home folder, from
+# the dotfiles folder - nothing intelligent going on here
+
+LINKED_FILES=("jshintrc" "slate" "tmux.conf" "vimrc" "zshrc")
+
+function main() {
+  for i in ${LINKED_FILES[@]} ; do
+    echo -s "$HOME/dotfiles/$i" "$HOME/.$i"
+  done
+  setup_vim
+}
+
+function setup_vim() {
+  echo $HOME/.vim
+  echo $HOME/.vim/backup
+  echo $HOME/.vim/tmp
+  echo $HOME/.vim/bundle
+  echo $HOME/.vim/undo
+  cd $HOME/.vim/bundle
+  git clone https://github.com/gmarik/vundle $HOME/.vim/bundle
+}
+
+main
